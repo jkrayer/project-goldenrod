cc14bb651b7cf7cd7c375db82d27e3ecdf096b58
Jim Krayer <jameskrayer@yahoo.com>
2026-01-12 18:50:46 -0500

    0.1.0 Remove Storybook and redux

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,15 +5,6 @@ import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
-import { fileURLToPath } from "node:url";
-import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
-import { playwright } from "@vitest/browser-playwright";
-const dirname =
-  typeof __dirname !== "undefined"
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
-
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [
     react({
@@ -46,27 +37,8 @@ export default defineConfig({
     projects: [
       {
         extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, ".storybook"),
-          }),
-        ],
-        test: {
-          name: "storybook",
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright({}),
-            instances: [
-              {
-                browser: "chromium",
-              },
-            ],
-          },
-          setupFiles: [".storybook/vitest.setup.ts"],
-        },
+        plugins: [],
+        test: {},
       },
     ],
   },
