cdc0fc6e490e7bf8673fcf61f3b812afda04e888
Jim Krayer <jameskrayer@yahoo.com>
2026-01-19 16:34:27 -0500

    0.1.0 Add toasts

--- frontend/src/pages/Session/Session.tsx
+++ frontend/src/pages/Session/Session.tsx
@@ -1,10 +1,27 @@
+import { Button, Container } from "@mui/material";
+import SnackbarProvider, { useSnackbar } from "./Toast/Toast";
 import packageJson from "../../../package.json";
 
 export default function Session() {
   return (
-    <div>
-      <h1>Session</h1>
-      <p>Alpha version {packageJson.version}</p>
-    </div>
+    // Add full page frame here
+    <SnackbarProvider>
+      <Container disableGutters={true}>
+        <Play />
+        <h1>Session</h1>
+        <p>Alpha version {packageJson.version}</p>
+      </Container>
+    </SnackbarProvider>
   );
 }
+
+// To be replaced by websocket messages
+const Play = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  return (
+    <Button onClick={() => enqueueSnackbar("This is a test message!")}>
+      Show Toast
+    </Button>
+  );
+};
